26b61663a95365bdfcd8d7089455cce9263832ca
evolF35 <maccyair@gmail.com>
2023-01-21 10:25:15 +0400

    ok ok

--- src/pages/Table.js
+++ src/pages/Table.js
@@ -15,33 +15,10 @@ import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
 import {ethers} from 'ethers'
 
-
-/*function createData(name, calories, fat, carbs, protein, price) {
-  return {
-    name,
-    calories,
-    fat,
-    carbs,
-    protein,
-    price,
-    history: [
-      {
-        date: '2020-01-05',
-        customerId: '11091700',
-        amount: 3,
-      },
-      {
-        date: '2020-01-02',
-        customerId: 'Anonymous',
-        amount: 1,
-      },
-    ],
-  };
-}*/
-
 function createData(modifiedTB) {
 
   console.log(modifiedTB);
+  if (!modifiedTB.zTOTBAL) return null;
 
   return {
     totalBalance: modifiedTB.zTOTBAL,
@@ -77,7 +54,7 @@ function Row(props) {
   const { row } = props;
   const [open, setOpen] = React.useState(false);
 
-  if (!row.totalBalance) return null;
+  if (!row) return null;
 
   return (
     <React.Fragment>
@@ -154,31 +131,30 @@ function Row(props) {
 
 Row.propTypes = {
   row: PropTypes.shape({
-    totalBalance: PropTypes.number.isRequired,
-    POSBalance: PropTypes.number.isRequired,
-    NEGBalance: PropTypes.number.isRequired,
-    SettlementPrice: PropTypes.number.isRequired,
-    SettlementDate: PropTypes.number.isRequired,
-    DecayRate: PropTypes.number.isRequired,
-    MaxRatio: PropTypes.number.isRequired,
-    MaxRatioDate: PropTypes.number.isRequired,
-    PastSettlementDate: PropTypes.bool.isRequired,
-    Condition: PropTypes.bool.isRequired,
-    DiscountRate: PropTypes.number.isRequired,
-    Withdraw: PropTypes.bool.isRequired,
-    details: PropTypes.arrayOf(
-      PropTypes.shape({
-        ContractAddress: PropTypes.string.isRequired,
-        OracleAddress: PropTypes.string.isRequired,
-        Name: PropTypes.string.isRequired,
-        Acronym: PropTypes.string.isRequired,
-        DestructionDate: PropTypes.number.isRequired,
-        POSAddress: PropTypes.string.isRequired,
-        NEGAddress: PropTypes.string.isRequired,
-      }),
-    ).isRequired,
+      totalBalance: PropTypes.string,
+      POSBalance: PropTypes.string,
+      NEGBalance: PropTypes.string,
+      SettlementPrice: PropTypes.string,
+      SettlementDate: PropTypes.string,
+      DecayRate: PropTypes.string,
+      MaxRatio: PropTypes.string,
+      MaxRatioDate: PropTypes.string,
+      PastSettlementDate: PropTypes.string,
+      Condition: PropTypes.string,
+      DiscountRate: PropTypes.string,
+      Withdraw: PropTypes.string,
+      details: PropTypes.arrayOf(PropTypes.shape({
+          ContractAddress: PropTypes.string,
+          OracleAddress: PropTypes.string,
+          Name: PropTypes.string,
+          Acronym: PropTypes.string,
+          DestructionDate: PropTypes.string,
+          POSAddress: PropTypes.string,
+          NEGAddress: PropTypes.string,
+      }))
   }).isRequired,
 };
+
 
 export default function CollapsibleTable(props) {
 
@@ -190,18 +166,26 @@ export default function CollapsibleTable(props) {
         <TableHead>
           <TableRow>
             <TableCell />
-            <TableCell>Dessert (100g serving)</TableCell>
-            <TableCell align="right">Calories</TableCell>
-            <TableCell align="right">Fat&nbsp;(g)</TableCell>
-            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+            <TableCell>Total Balance</TableCell>
+            <TableCell align="right"> POS Balance </TableCell>
+            <TableCell align="right"> NEG Balance </TableCell>
+            <TableCell align="right"> Settlement Price </TableCell>
+            <TableCell align="right"> Decay Rate </TableCell>
+            <TableCell align="right"> Max Ratio </TableCell>
+            <TableCell align="right"> Max Ratio Date </TableCell>
+            <TableCell align="right"> PastSettlement Date </TableCell>
+            <TableCell align="right"> Condition </TableCell>
+            <TableCell align="right"> Discount Rate </TableCell>
+            <TableCell align="right"> Withdraw </TableCell>
+
           </TableRow>
         </TableHead>
         <TableBody>
-          {rows.map((row) => (
-            <Row key={row.contractAddress} row={row} />
-          ))}
-        </TableBody>
+{rows.map((row) => {
+   const data = createData(row);
+   return data ? <Row key={data.contractAddress} row={data} /> : null;
+})}
+</TableBody>
       </Table>
     </TableContainer>
   );
